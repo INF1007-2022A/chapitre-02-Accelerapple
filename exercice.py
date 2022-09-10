a9edf1514ee66f1ece667bb6046b1fc9774b348e
mberg <javier.berger@polymtl.com>
2022-09-10 00:23:57 -0400

    test3 1

--- exercice.py
+++ exercice.py
@@ -1,21 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
+ok = True
 
 
-if __name__ == '__main__':
-    mots = [
-        'riz',
-        'cours',
-        'voiture',
-        'oiseau',
-        'bonjour',
-        'églantier',
-        'arbre'
-    ]
-    for i in range(len(mots)):
-        mots[i] = majuscule(mots[i])
+def majusculation(texte):
+    lettres = list(texte)
+    output = ""
+    for i in lettres:
+        dec = ord(i)
+        if 97 <= dec <= 122:
+            maj = dec - 32
+            output += (chr(maj))
+        else:
+            output += i
 
-    print(mots)
+    print(output)
+
+
+def minuculation(texte):
+    lettres = list(texte)
+    output = ""
+    for i in lettres:
+        dec = ord(i)
+        if 65 <= dec <= 90:
+            maj = dec + 32
+            output += (chr(maj))
+        else:
+            output += i
+
+    print(output)
+
+
+def choice():
+    global ok
+
+    while True:
+        choix = input("maj ou min?: ")
+        if choix == "maj" or choix == "min":
+            break
+        elif choix == "end":
+            print("Bye")
+            ok = False
+            print("\n", "last words?")
+            break
+        else:
+            print("bruh")
+
+    entre = input("ecrire ici: ")
+    if choix == "maj":
+        majusculation(entre)
+    elif choix == "min":
+        minuculation(entre)
+    elif not ok:
+        choix = "End"
+
+    print("end with option: ", choix, "\n")
+
+
+while ok:
+    choice()
+
